bdeafaf8f2d7d2f2ec921b48045261544d02fc58
Christophe Schmitz <cschmitz@redhat.com>
2023-07-11 08:33:05 +1000

    get cluster info

--- OCP-Demo/get-cluster-info.sh
+++ OCP-Demo/get-cluster-info.sh
@@ -5,10 +5,3 @@ aa=$(grep "Access the OpenShift web-console here:" ./$MYDIR/.openshift_install.l
 echo "Dashboard: ${aa::-1}"
 echo "Username : kubeadmin"
 echo "Password : $(cat $MYDIR/auth/kubeadmin-password)"
-
-ARGO_SERVER=$(KUBECONFIG=$MYDIR/auth/kubeconfig oc get route openshift-gitops-server -n openshift-gitops -o jsonpath='{.spec.host}')
-echo "argo url : https://${ARGO_SERVER}"
-echo "argo ep  : $ARGO_SERVER"
-
-ARGO_PASSWORD=$(KUBECONFIG=$MYDIR/auth/kubeconfig oc get secret openshift-gitops-cluster -n openshift-gitops -o json | jq -r '.data."admin.password"' | base64 -d)
-echo "argo pwd : $ARGO_PASSWORD"
