9278f9804f5706d49119c113b560918d8186fc35
Christophe Schmitz <cschmitz@redhat.com>
2023-07-10 20:00:09 +1000

    add destroy cluster script

---
+++ OCP-Demo/destroy-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+source ./env.sh
+
+function delete {
+    date
+    cd $MYDIR
+    openshift-install destroy cluster
+}
+
+function delete_argo_cluster {
+    # doesnt' work, permission issue
+    LIST=$(argocd cluster list --config ~/.aws/argo-config -o json | jq .[].name -r)
+    echo "$LIST" | grep "${ARGO_CLUSTER_NAME}"
+    if [ $? -eq 0 ]
+    then
+        echo "Removing the cluster $ARGO_CLUSTER_NAME to argocd"
+        argocd cluster rm --config ~/.aws/argo-config $ARGO_CLUSTER_NAME
+    fi
+}
+
+MYDIR=$MYDIR delete
+#ARGO_CLUSTER_NAME=$ARGO_CLUSTER_NAME delete_argo_cluster
