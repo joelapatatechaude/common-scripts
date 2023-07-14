7f868e4ab9d08cd234c3861b20d9c035138dacf7
Christophe Schmitz <cschmitz@redhat.com>
2023-07-14 20:07:49 +1000

    update get-cluster-info

--- OCP-Demo/functions-get-cluster-info.sh
+++ OCP-Demo/functions-get-cluster-info.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 function get_cluster_info {
-    aa=$(grep "Access the OpenShift web-console here:" ./$MYDIR/.openshift_install.log | tail -1 | awk -F 'web-console here: ' '{print $2}' )
+    if [ $CLUSTER_NAME == "crc" ];
+    then
+	aa=https://console-openshift-console.apps-crc.testing/
+    else
+	aa=$(grep "Access the OpenShift web-console here:" ./$MYDIR/.openshift_install.log | tail -1 | awk -F 'web-console here: ' '{print $2}' )
+    fi
     echo "Dashboard: ${aa::-1}"
     echo "Username : kubeadmin"
     echo "Password : $(cat $MYDIR/auth/kubeadmin-password)"
