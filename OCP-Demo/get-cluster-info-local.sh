7fa3904f8d5b4ffb4836f3ef6a6e216f73fcc083
Christophe Schmitz <cschmitz@redhat.com>
2023-11-30 14:51:35 +1100

    add local version

---
+++ OCP-Demo/get-cluster-info-local.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+source ./env-local.sh
+source <(curl -s https://raw.githubusercontent.com/joelapatatechaude/common-scripts/main/OCP-Demo/functions-get-cluster-info.sh)
+
+get_cluster_info
